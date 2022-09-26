48220171c2f2d211d8290b67d0202fa8604df8ca
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2022-09-25 21:40:18 -0300

    add component input in App.tsx

--- devagram/App.tsx
+++ devagram/App.tsx
@@ -2,8 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "@expo-google-fonts/inter";
 import AppLoading from "expo-app-loading";
 import Button from "./src/_components/Button";
+import Input from "./src/_components/Input";
+import { useState } from "react";
 
 export default function App() {
+  const [email, setEmail] = useState<string>("");
   const [fontsLoaded] = useFonts({
     biennale: require("./assets/fonts/biennale-regular.otf"),
     "biennale-bold": require("./assets/fonts/biennale-bold.otf"),
@@ -16,6 +19,11 @@ export default function App() {
         placeholder="Clique aqui"
         loading={false}
         disabled={false}
+      />
+      <Input
+        onChangeText={(e: string) => setEmail(e)}
+        placeholder={"Digite seu E-mail"}
+        value={email}
       />
     </View>
   ) : (
